c88e17af702c4e4fbbf6f0e3b2f0a445c294c9db
SilurianYang <1606726660@qq.com>
2020-06-24 12:16:20 +0800

    + auto build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ cp -avx ./* ./npm-package
 rm -rf ./npm-package/node_modules
 rm -rf ./npm-package/example
 rm -rf ./npm-package/npm-package
+rm -rf ./npm-package/package-lock.json
+rm -rf ./npm-package/build.sh
